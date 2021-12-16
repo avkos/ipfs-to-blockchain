0a1193f289d31506e2dc284d6b85da3329faab73
oleksiikos <alex@mediatech.ai>
2021-12-16 22:46:17 +0200

    remove js files

--- contract/migrations/1_initial_migration.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Migrations = artifacts.require("Migrations");
-const IpfsDataSaver = artifacts.require("IpfsDataSaver");
-module.exports = function (deployer) {
-    deployer.deploy(Migrations);
-    deployer.deploy(IpfsDataSaver);
-};
--- main/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const module_controller_1 = __importDefault(require("./modules/module.controller"));
-const controller = new module_controller_1.default();
-controller.run().catch(console.error);
